22f2deb1d7cdfad864ec3c86278e9041460bf9f6
iJ4d3 <48686409+iJ4d3@users.noreply.github.com>
2026-05-22 20:04:08 -0400

    Add prototype summary output structure

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-print("Welcome to PlainPath!")
+print("--- Welcome to PlainPath! ---")
 
 policy = input("\nPaste Policy Text Here\n")
 
 print("\nProcessing...\n")
 
-print("\nSummary:\n")
-print(policy)
+summary = policy[:150]
+
+print("--- SUMMARY ---")
+print(summary)
+
+print("\n--- Action Items ---")
+print("- Review Policy")
+print("- Determine Applicability")
+
+print("\n--- Impacted Users ---")
+print("- Employees")
