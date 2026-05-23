8d2218c7348b23981f3871e5001f7e19b90fe5d5
iJ4d3 <48686409+iJ4d3@users.noreply.github.com>
2026-05-23 17:31:19 -0400

    Refactor prototype into functions

--- app.py
+++ app.py
@@ -1,29 +1,39 @@
+# define functions
+
+# function to summarize provided policy text
+def summarize(policy):
+    return policy[:150]
+
+# function to find impacted users based on keywords in the policy text
+def find_impacted_users(policy):
+    impacted = []
+    policy_lower = policy.lower()
+
+    if "employees" in policy_lower:
+        impacted.append("Employees")
+    if "managers" in policy_lower:
+        impacted.append("Managers")
+    if "contractors" in policy_lower:
+        impacted.append("Contractors")
+
+    return impacted
+
+# start of main program
 print("--- Welcome to PlainPath! ---")
 
+# get policy text input from user
 policy = input("\nPaste Policy Text Here\n")
 
 print("\nProcessing...\n")
 
-summary = policy[:150]
+# define variables to hold summary and impacted users
+summary = summarize(policy)
+impacted_users = find_impacted_users(policy)
 
+# print summary and impacted users
 print("--- SUMMARY ---")
 print(summary)
 
-print("\n--- Action Items ---")
-print("- Review Policy")
-print("- Determine Applicability")
-
-print("\n--- Impacted Users ---")
-
-impacted = []
-policy_lower = policy.lower()
-
-if "employees" in policy_lower:
-    impacted.append("Employees")
-if "managers" in policy_lower:
-    impacted.append("Managers")
-if "contractors" in policy_lower:
-    impacted.append("Contractors")
-
-for group in impacted:
-    print(f"- {group}")
+print("\n--- IMPACTED USERS ---")
+for user in impacted_users:
+    print(f"- {user}")
